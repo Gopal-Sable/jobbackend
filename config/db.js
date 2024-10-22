363d27a3c8ed2db586b1a45865af4d48deed81b0
Gopal Sable <sablegopal123@gmail.com>
2024-10-22 11:28:57 +0530

    log add for deployement checking

--- config/db.js
+++ config/db.js
@@ -6,8 +6,12 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     logger.info('MongoDB connected');
+    console.log("MongoDB connected");
+    
   } catch (err) {
     logger.error('MongoDB connection failed:', err);
+    console.log("Failed to connect: ",err);
+    
     process.exit(1);
   }
 };
